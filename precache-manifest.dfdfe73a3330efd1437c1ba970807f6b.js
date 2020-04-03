2cd71e79140d563aa5753cf204078846bc4427f8
kusumitha <kusumithakrishnan01@gmail.com>
2020-04-03 16:34:32 +0530

    Updates

---
+++ precache-manifest.dfdfe73a3330efd1437c1ba970807f6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fa434f6bcd98ca6271fc82e0c1623da",
+    "url": "/static/media/logo.3fa434f6.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "41975e6c57eb0b4f1576",
+    "url": "/static/js/main.f3a8f7d4.chunk.js"
+  },
+  {
+    "revision": "6752c7816269d03f6d09",
+    "url": "/static/js/2.7250e5a2.chunk.js"
+  },
+  {
+    "revision": "41975e6c57eb0b4f1576",
+    "url": "/static/css/main.25d74c1c.chunk.css"
+  },
+  {
+    "revision": "52bf3403ae468c6aa5426343322cadcc",
+    "url": "/index.html"
+  }
+];
